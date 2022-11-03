ba006250bbb66892aec253d615bce7c8cc892f6a
DhanshreePatil01 <patildhanshrees6112@gmail.com>
2022-11-04 00:58:04 +0530

    Added : UC4 Repeat till the player reaches to 100.

--- src/com/Bridgelabz/SnakeAndLadder.java
+++ src/com/Bridgelabz/SnakeAndLadder.java
@@ -14,9 +14,17 @@ public class SnakeAndLadder {
 	System.out.println("Dice Rolled At :"+roll_dice);
 	
 	int position=0;
+	int dice_count = 0;
+	int condition = 10;
+	while (condition > 0) {
 	int options=(int)(Math.random()*3+1);
 	System.out.println("option is: "+options);
 	
+	if (roll_dice > 0) 
+	{
+		dice_count++;
+	}
+
 	switch(options)
 	{
 	case 1:
@@ -26,6 +34,14 @@ public class SnakeAndLadder {
 	case 2:
 		System.out.println("There is Ladder");
 		position=position+roll_dice;
+		if (position < 0)
+		{
+			position = 0;
+		} 
+		else 
+		{
+			position = position;
+		}
 		break;
 	case 3:
 		System.out.println("There is Snake");
@@ -35,5 +51,20 @@ public class SnakeAndLadder {
 		System.out.println("Roll dice again");
 		break;
 		}
+	
+	System.out.println("Position is : " + position);
+
+	System.out.println("-------------------------");
+
+	if (position == 100) 
+	{
+		System.out.println("Player is winner");
+		break;
+	} 
+	else
+	{
+		System.out.println("roll a dice again");
+	}
+	}
  }
 }
