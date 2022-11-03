6527db1624205afe8fbfb2328f04d3173274bf59
DhanshreePatil01 <patildhanshrees6112@gmail.com>
2022-11-04 00:36:41 +0530

    Added : UC3 Player checks for a Option as No Play ,Ladder or Snake.

--- src/com/Bridgelabz/SnakeAndLadder.java
+++ src/com/Bridgelabz/SnakeAndLadder.java
@@ -12,5 +12,28 @@ public class SnakeAndLadder {
 	
 	int roll_dice=(int)(Math.random()*6+1);
 	System.out.println("Dice Rolled At :"+roll_dice);
+	
+	int position=0;
+	int options=(int)(Math.random()*3+1);
+	System.out.println("option is: "+options);
+	
+	switch(options)
+	{
+	case 1:
+		System.out.println("No Play");
+		position=position;
+		break;
+	case 2:
+		System.out.println("There is Ladder");
+		position=position+roll_dice;
+		break;
+	case 3:
+		System.out.println("There is Snake");
+		position=position-roll_dice;
+		break;
+	default:
+		System.out.println("Roll dice again");
+		break;
+		}
  }
 }
