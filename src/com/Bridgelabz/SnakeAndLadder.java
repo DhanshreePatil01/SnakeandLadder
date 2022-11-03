e5084e7710b9649f118359386e95abbb3cb3874f
DhanshreePatil01 <patildhanshrees6112@gmail.com>
2022-11-04 00:18:43 +0530

    Added : UC1 Player starts at position 0.

---
+++ src/com/Bridgelabz/SnakeAndLadder.java
@@ -0,0 +1,12 @@
+package com.Bridgelabz;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+		
+	System.out.println("Starting of Snake and Ladder Problem");
+	
+	System.out.println("Single player start at position 0");
+	
+ }
+}
